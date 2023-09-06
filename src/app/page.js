360834423dc4a42a1a3f36405511c34969572d02
Roberto Menezes <roberto.menesantos@gmail.com>
2023-09-06 20:50:36 -0300

    teste

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,7 @@
-import Link from "next/link";
-import CaixaMensagem from "../Componentes/CaixaMensagem/page";
-import Headers from "../Componentes/Header/page";
-import InputTexto from "../Componentes/InputTexto/page";
-
 export default function Home() {
     return (
         <div>
-            <Headers/>
-            <main className="mb-20">
-                <CaixaMensagem envio="bot" mensagem={"Olá, sou o ChatBot e vou ajudar você com a vistoria. Podemos começar?"}/>
-                <CaixaMensagem envio="user" mensagem={"Sim"}/>
-                <CaixaMensagem envio="bot" mensagem={"Tudo Bem, antes de começarmos você possue alguma duvida"}/>
-                <CaixaMensagem envio="user" mensagem={"Não"}/>
-                <CaixaMensagem envio="bot" mensagem={"Tudo Bem, vamos começar"}/>
-                <CaixaMensagem envio="user" mensagem={"Sim"}/>
-            </main>
-            <InputTexto/>
+            <h1>oi</h1>
         </div>
     )
 }
