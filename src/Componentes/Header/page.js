230d750080891775dbbe7626d6f1830bfcdcf336
Roberto Menezes <roberto.menesantos@gmail.com>
2023-09-08 10:18:01 -0300

    feat: criação das paginas chatBot, vistoriaConcluida

--- src/Componentes/Header/page.js
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Headers(){
-    return(
-        <header className="w-full flex justify-center border-b-2">
-            <Image className="aspect-auto" src="/imgs/logo-porto.webp" alt="Logo da Porto Seguro" width={150} height={0}/>
-        </header>
-    )
-}
--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,18 @@
 import Link from "next/link";
-import CaixaMensagem from "../Componentes/CaixaMensagem/page";
-import Headers from "../Componentes/Header/page";
-import InputTexto from "../Componentes/InputTexto/page";
+import Botao from "./Componentes/Botao";
+import Header from "./Componentes/Header";
 
 export default function Home() {
     return (
-        <div>
-            <Headers/>
-            <main className="mb-20">
-                <CaixaMensagem envio="bot" mensagem={"Olá, sou o ChatBot e vou ajudar você com a vistoria. Podemos começar?"}/>
-                <CaixaMensagem envio="user" mensagem={"Sim"}/>
-                <CaixaMensagem envio="bot" mensagem={"Tudo Bem, antes de começarmos você possue alguma duvida"}/>
-                <CaixaMensagem envio="user" mensagem={"Não"}/>
-                <CaixaMensagem envio="bot" mensagem={"Tudo Bem, vamos começar"}/>
-                <CaixaMensagem envio="user" mensagem={"Sim"}/>
+        <>
+            <Header/>
+            <main className="flex flex-col items-center space-y-16 min-h-[90vh] justify-center">
+                <h1 className="font-bold text-4xl">Bem-Vindo</h1>
+                <p className="max-w-[200px] font-semibold text-center">Clique em avançar para darmos inicio ao processo de vistoria</p>
+                <Link href="/chatBot">
+                    <Botao texto="Avançar"/>
+                </Link>
             </main>
-            <InputTexto/>
-        </div>
+        </>
     )
 }
