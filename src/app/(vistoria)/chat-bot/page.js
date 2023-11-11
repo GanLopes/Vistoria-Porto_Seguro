682d301b0a4e3835d819bca4912c8d60ba5beceb
Roberto Menezes <roberto.menesantos@gmail.com>
2023-11-11 14:34:36 -0300

    refactor:

--- src/app/(vistoria)/chat-bot/page.js
+++ src/app/(vistoria)/chat-bot/page.js
@@ -6,13 +6,13 @@ import ChatBotFooter from "/src/componentes/ChatBotFooter.js";
 import Mensagem from "/src/componentes/Mensagem.js";
 import mensagens from "/src/utils/mensagens.json";
 
-export default function chatBot() {
+export default function ChatBot() {
     const [isDesabled, setIsDesabled] = useState(false);
 
     return (
         <div className="max-w-sm flex flex-col md:max-w-2xl mx-auto space-y-5 pt-4">
-            {mensagens.map((m) => (
-                <Mensagem {...m} />
+            {mensagens.map((m, i) => (
+                <Mensagem key={i} {...m} />
             ))}
             <ChatBotFooter href={"/envio-fotos"} desabilitado={isDesabled} />
         </div>
