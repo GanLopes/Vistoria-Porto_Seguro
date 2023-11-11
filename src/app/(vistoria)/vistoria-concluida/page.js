b0c89bb010273262c2296f76a14ab8b642440c08
Roberto Menezes <roberto.menesantos@gmail.com>
2023-11-11 14:00:48 -0300

    refactor:

--- src/app/(vistoria)/vistoria-concluida/page.js
+++ src/app/(vistoria)/vistoria-concluida/page.js
@@ -1,15 +1,15 @@
-import Botao from "/src/componentes/Botao.js";
 
 export default function Home() {
     return (
         <div className="flex items-center justify-center h-full md:max-w-xl mx-auto">
-            <div className="border-2 border-sky-600 md:border-zinc-300 md:hover:border-sky-600 md:hover:bg-sky-50 bg-sky-50 md:bg-transparent transition-colors duration-200 rounded-xl p-5 flex flex-col text-center space-y-10 w-full">
+            <div className="border-2 border-sky-600 md:border-zinc-300 md:hover:border-sky-600 md:hover:bg-sky-50 bg-sky-50 md:bg-transparent transition-colors duration-200 rounded-xl p-5 flex flex-col text-center space-y-10 w-full h-64 items-center justify-center">
                 <h1 className="text-3xl font-bold">Vistoria Concluída</h1>
                 <p className="mx-auto">
-                    A vistoria foi realizada com sucesso!<br/>
-                    Agora você poderá usufruir <br/>do melhor seguro do Brasil
+                    A vistoria foi realizada com sucesso!
+                    <br />
+                    Agora você poderá usufruir <br />
+                    do melhor seguro do Brasil
                 </p>
-                <Botao href="/chat-bot" texto="Avançar" />
             </div>
         </div>
     );
