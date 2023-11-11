a4e01962da5f4c1503a93d1651263b1ec84851ed
Roberto Menezes <roberto.menesantos@gmail.com>
2023-11-11 14:08:15 -0300

    refactor:

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-export const content = [
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
-];
-export const theme = {
-    extend: {},
-};
-export const plugins = [];
+module.exports = {
+    content: [
+        './src/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
